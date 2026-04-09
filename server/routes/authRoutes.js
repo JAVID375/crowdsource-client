4d6a2244c33620a2b717ce4156ec000d11d05d0f
shaik javid <shaikameena375@gmail.com>
2026-04-09 11:52:46 +0530

    Updated forgot password with Brevo SMTP

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -78,15 +78,12 @@ router.post("/forgot-password", async (req, res) => {
 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
   }
 });
 const message = `
@@ -96,7 +93,7 @@ const message = `
 `;
 
 await transporter.sendMail({
-  from: process.env.EMAIL_USER,
+  from: process.env.SMTP_USER,
   to: user.email,
   subject: "Password Reset",
   html: message
